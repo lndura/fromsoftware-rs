ee5ae21620e948a6700d7fd2e8b414b2c3b9b632
axd1x8a <sign@axd1x8a.me>
2026-05-26 02:11:34 +0300

    Add a note about encoding of FNString

--- crates/eldenring/src/from_net/stl.rs
+++ crates/eldenring/src/from_net/stl.rs
@@ -1,4 +1,8 @@
 use crate::from_net::FnAllocatorProxy;
 
-pub type FNVector<T> = fromsoftware_shared_stl::Vector<T, FnAllocatorProxy>;
-pub type FNString = fromsoftware_shared_stl::BasicString<u8, FnAllocatorProxy>;
+use fromsoftware_shared_stl::{BasicString, Vector};
+
+pub type FNVector<T> = Vector<T, FnAllocatorProxy>;
+/// [`BasicString`] that uses [`FnAllocatorProxy`] for all of it's operations.
+/// Can be either UTF-8 or ShiftJIS, depending on usage in the game.
+pub type FNString = BasicString<u8, FnAllocatorProxy>;
